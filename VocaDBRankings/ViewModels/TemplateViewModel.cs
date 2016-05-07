ec088aaa5017e24d27bb466b3887083e824b77bb
Riipah Furude <riipah@vocadb.net>
2016-05-07 16:07:57 +0300

    only get main PVs

--- VocaDBRankings/ViewModels/TemplateViewModel.cs
+++ VocaDBRankings/ViewModels/TemplateViewModel.cs
@@ -1,8 +1,30 @@
-﻿using VocaDBRankings.DataContracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VocaDBRankings.DataContracts;
 
 namespace VocaDBRankings.ViewModels {
 
 	public class TemplateViewModel {
+
+		private PVContract GetMainPV(PVContract[] pvs, PVService service) {
+
+			pvs = pvs.Where(p => p.Service == service).ToArray();
+
+			return pvs.FirstOrDefault(p => p.PVType == PVType.Original) ?? pvs.FirstOrDefault(p => p.PVType == PVType.Reprint) ?? pvs.FirstOrDefault(p => p.PVType == PVType.Other);
+
+		}
+
+		public PVContract[] GetMainPVs(SongForApiContract song) {
+
+			return Enum.GetValues(typeof(PVServices))
+				.Cast<PVServices>()
+				.Where(service => (song.PVServices & service) == service)
+				.Select(service => GetMainPV(song.PVs, (PVService)service))
+				.Where(pv => pv != null)
+				.ToArray();
+
+		}
 
 		public SongForApiContract[] OtherSongs { get; set; }
 
